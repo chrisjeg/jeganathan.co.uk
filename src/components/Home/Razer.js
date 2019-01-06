f11ae3d9c2b7fc1be347859891b9e8269c97ff0b
chrisjeg <chris.jeganathan@gmail.com>
2019-01-06 20:47:59 +0000

    Razer now uses effects and state

--- src/components/Home/Razer.js
+++ src/components/Home/Razer.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import classNames from "classnames";
 import chunk from "lodash.chunk";
 import "./Razer.scss";
@@ -6,44 +6,37 @@ import "./Razer.scss";
 const KEYS = "RAZERJS";
 const KEYS_PER_ROW = 5;
 const chunkedKeys = chunk(KEYS.split(""), KEYS_PER_ROW);
-class RazerSection extends Component {
-  constructor() {
-    super();
-    this.state = {
-      activeKey: 0
-    };
-  }
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState(prev => ({
-        activeKey: (prev.activeKey + 1) % KEYS.length
-      }));
+function RazerSection() {
+  const [activeKey, setActiveKey] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setActiveKey((activeKey + 1) % KEYS.length);
     }, 600);
-  }
+  });
 
-  render() {
-    return (
-      <div className="razer-animation drop-transition">
-        <div className="razer-board">
-          {chunkedKeys.map((characters, i) => (
-            <div className="keys" key={i}>
-              {characters.map((value, j) => (
-                <div
-                  className={classNames("key", "key" + j, {
-                    pressed: i * KEYS_PER_ROW + j === this.state.activeKey
-                  })}
-                  key={j}
-                >
-                  {value}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+  return (
+    <div className="razer-animation drop-transition">
+      <div className="razer-board">
+        {chunkedKeys.map((characters, i) => (
+          <div className="keys" key={i}>
+            {characters.map((value, j) => (
+              <div
+                className={classNames("key", "key" + j, {
+                  pressed: i * KEYS_PER_ROW + j === activeKey
+                })}
+                key={j}
+              >
+                {value}
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default RazerSection;
