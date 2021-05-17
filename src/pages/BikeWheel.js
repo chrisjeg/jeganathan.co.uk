a9e4c9fb3f2ea5365f366cfb2a7856f4a9ef8daf
Chris Jeg <4092952+chrisjeg@users.noreply.github.com>
2021-05-17 17:12:24 +0100

    I think its done

--- src/pages/BikeWheel.js
+++ src/pages/BikeWheel.js
@@ -1,5 +1,8 @@
 import React from "react";
+import regression from "regression";
 import "./BikeWheel.scss";
+
+const startTime = Date.now();
 
 const BikeWheel = () => {
   const [rotation, setRotation] = React.useState(0);
@@ -7,11 +10,9 @@ const BikeWheel = () => {
   const previousTimeRef = React.useRef();
   const [device, setDevice] = React.useState();
 
-  const revolutionData = React.useRef({
-    rpm: 0,
-    totalRevolutions: 0,
-    lastWheelTime: 0,
-  });
+  const revolutionData = React.useRef(null);
+  const noUpdateCounter = React.useRef(0);
+  const storedEvents = React.useRef([]);
 
   const setupDevice = React.useCallback(async () => {
     await navigator.bluetooth.getAvailability();
@@ -38,6 +39,23 @@ const BikeWheel = () => {
       }
 
       const previous = revolutionData.current;
+      if (previous == null) {
+        revolutionData.current = {
+          rpm: 0,
+          lastWheelTime: curr.lastWheelTime,
+          totalRevolutions: curr.totalRevolutions,
+        };
+        return;
+      }
+
+      if (curr.lastWheelTime === previous.lastWheelTime) {
+        if (noUpdateCounter.current < 2) {
+          noUpdateCounter.current++;
+          return;
+        }
+      } else {
+        noUpdateCounter.current = 0;
+      }
 
       const revDelta = curr.totalRevolutions - previous.totalRevolutions;
       let timeDelta = curr.lastWheelTime - previous.lastWheelTime;
@@ -45,20 +63,56 @@ const BikeWheel = () => {
         timeDelta = curr.lastWheelTime + 64 - previous.lastWheelTime;
       }
       const newData = {
-        rpm: timeDelta === 0 ? 0 : revDelta * (60 / timeDelta),
+        rpm:
+          timeDelta === 0 || previous.lastWheelTime === 0
+            ? 0
+            : revDelta * (60 / timeDelta),
         totalRevolutions: curr.totalRevolutions,
         lastWheelTime: curr.lastWheelTime,
       };
-      console.log(newData);
+      const rpmDelta = previous.rpm - newData.rpm;
+
+      if (rpmDelta < 0) {
+        storedEvents.current = [];
+      } else if (rpmDelta < 10 && rpmDelta > 0.5 && newData.rpm !== 0) {
+        storedEvents.current.push([Date.now() - startTime, newData.rpm]);
+      }
+
+      if (
+        (newData.rpm === 0 || rpmDelta > 10) &&
+        storedEvents.current.length > 0
+      ) {
+        const [, guess] = regression
+          .linear(storedEvents.current, { order: 5, precision: 4 })
+          .predict(Date.now() - startTime);
+        if (guess > 0) {
+          revolutionData.current = {
+            ...newData,
+            rpm: guess,
+          };
+          return;
+        } else {
+          // Guesses are now no longer valid
+          storedEvents.current = [];
+        }
+      }
+
+      console.log(rpmDelta);
+
       revolutionData.current = newData;
     });
+
     return setDevice(newDevice);
   }, []);
 
   const animate = (time) => {
-    setRotation(
-      revolutionData.current.rpm !== 0
-        ? (time % 360) / (revolutionData.current.rpm / 60000)
+    setRotation((prevRotation) =>
+      revolutionData.current != null
+        ? (prevRotation +
+            (time - previousTimeRef.current) *
+              (revolutionData.current.rpm / 60000) *
+              360) %
+          360
         : 0
     );
     previousTimeRef.current = time;
@@ -140,7 +194,13 @@ const WheelSegment = ({
         <line x1="0" y1="0" x2={r} y2="0" stroke="black" stroke-width="3" />
       </g>
       <g transform={`rotate(${segmentSize * (segmentNumber + 0.5)})`}>
-        <text x={r / 3} y="0" alignmentBaseline="central" textLength={r * 0.4}>
+        <text
+          x={r / 3}
+          y="0"
+          alignmentBaseline="central"
+          textLength={r * 0.4}
+          rotate={-90}
+        >
           {text}
         </text>
       </g>
